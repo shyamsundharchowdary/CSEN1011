5b03708fe308951b0e156896eeb39c87b4f312dc
shyamsundharchowdary <93367759+shyamsundharchowdary@users.noreply.github.com>
2021-11-02 13:56:06 +0530

    Add files via upload

---
+++ shyam.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+   string raptor_prompt_variable_zzyz;
+   ?? b;
+   ?? result;
+   ?? a;
+
+   raptor_prompt_variable_zzyz ="ENTER A NUMBER";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> A;
+   raptor_prompt_variable_zzyz ="ENTER ANOTHER NUMBER";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> B;
+   RESULT =A+B;
+   cout << " THE RESULT OF THE ADDITION IS"+RESULT << endl;
+   return 0;
+}
