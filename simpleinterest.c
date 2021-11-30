6cb5651397fc814a1406f13efc61b187cd8fb070
shyamsundharchowdary <93367759+shyamsundharchowdary@users.noreply.github.com>
2021-11-30 14:20:32 +0530

    Add files via upload

---
+++ simpleinterest.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+   float  p,t,r,si,ci;
+   scanf("%f", &p);
+   scanf("%f", &t);
+   scanf("%f", &r);
+   si=(p*t*r)/100;
+   ci=p*(((1+r)/100)*t);
+   printf("simple interest %f", si);
+   printf("compound interest %f, ci");
+
+}
