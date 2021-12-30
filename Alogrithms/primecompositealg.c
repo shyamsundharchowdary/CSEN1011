b66d3857690b2766d01eb43cda62a7a77174a30b
shyamsundharchowdary <93367759+shyamsundharchowdary@users.noreply.github.com>
2021-12-30 14:23:02 +0530

    Create primecompositealg.c

---
+++ Alogrithms/primecompositealg.c
@@ -0,0 +1,7 @@
+step1: find whether the number is prime or composite
+step2: accept a number ( greather than 2)
+step3: intialize counter with value 2
+step4: divide the number with counter value,
+       if the remainder is zero display that positive number is composite 
+       else impilment the counter if the remainder is not zero and repeat step 3 again 
+       
