93d977fba4e0447143858dbb208fc57182053901
shyamsundharchowdary <93367759+shyamsundharchowdary@users.noreply.github.com>
2022-02-24 10:49:37 +0530

    Add files via upload

---
+++ Module3/squareroot.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+printf()
+ int main() 
+ {
+	 	
+	double number = 67.5,result;
+	result = sqrt(number);
+	printf("square root of %.2lf=%.2lf\n",number,result);
+	return 0;
+}
